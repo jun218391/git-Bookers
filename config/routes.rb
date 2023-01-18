5388aec0fad34a3354473bc5d309511b40cc0ab4
jun218391 <122144759+jun218391@users.noreply.github.com>
2023-01-18 00:37:14 +0000

    [add]3-13

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 #     Prefix Verb   URI Pattern                   Controller#Action
 #     books GET    /books(.:format)                books#index
 #           POST   /books(.:format)                books#create
-#  new_book GET    /books/new(.:format)           books#new
+#  new_book GET    /books/new(.:format)            books#new
 # edit_book GET    /books/:id/edit(.:format)       books#edit
-#      book GET    /books/:id(.:format)           books#show
+#      book GET    /books/:id(.:format)            books#show
 #           PATCH  /books/:id(.:format)            books#update
 #           PUT    /books/:id(.:format)            books#update
 #           DELETE /books/:id(.:format)            books#destroy
